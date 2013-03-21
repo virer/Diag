0a630cb1630b22b787865708b899f2386737f292
Sebastien Caps <sebastien.caps@guardis.com>
2013-03-21 14:30:20 +0100

    added ram percent

--- diag.sh
+++ diag.sh
@@ -4,7 +4,7 @@ NC='\e[0m' # No Color
 echo -e "${red}Shared memory, Semaphores, and Message queues:${NC} "
 ipcs -ma
 echo -e "${red}Non ''sleeping'' process:${NC} "
-ps -eo %cpu,pid,vsz,args,wchan | egrep -v '^ 0.0|^ 0.1'
+ps -eo %cpu,%mem,pid,vsz,args,wchan | egrep -v '^ 0.0|^ 0.1'
 echo -ne "\n${red}Nbr of openned files / max files open:${NC} "
 cat /proc/sys/fs/file-nr | awk '{ print $1 "/" $3 } '
 echo -e "\n${red}Memory usage in Megabytes:${NC} "
